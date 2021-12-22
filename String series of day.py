19f03418aead288498e0a82e6dea7524f80690a8
Manmay <manmaychakarborty@gmail.com>
2021-12-22 22:47:10 +0530

    accurate one..

--- String series of day.py
+++ String series of day.py
@@ -1,20 +1,20 @@
 # # SUNDAY,MONDAY,WEDNESDAY,SATURDAY...…….n terms...
 li=["SUNDAY","MONDAY","TUESDAY","WEDNESDAY","THURSDAY","FRIDAY","SATURDAY"]
 n=int(input("Enter number of terms: "))
-r=0
 a=0
+r=0
 b=1
 for i in range(n):
+    if a==21:
+        a,r,b=0,0,1
     a=a+r
     r=r+1
-    if(a>6):
-        a=a-(7*b)
-        print(li[a],end=' ')
-        a=a+(7*b)
-        b=b+1
-    elif(a<=6):
-        print(li[a],end=' ')
-        
+    if(a<=6):
+        print(li[a])
+    else:
+        print(li[a-7*b])
+        b+=1
+print("..........................The End..................")
    
     
 
