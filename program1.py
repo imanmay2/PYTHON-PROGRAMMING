f7f36d38db391b4136ab7b32e7a2b315c6646b14
Manmay <manmaychakarborty@gmail.com>
2022-01-11 22:54:26 +0530

    BOT

--- program1.py
+++ program1.py
@@ -1,30 +1,12 @@
-'''You are required to write a program to sort the (name, age, height) tuples
-by ascending order where name is string, age and height are
-numbers. The tuples are input by console. The sort
-criteria is:
-1: Sort based on name;
-2: Then sort based on age;
-3: Then sort by score.
-The priority is that name > age > score.
-
-If the following tuples are given as
-input to the program:
-Tom,19,80
-John,20,90
-Jony,17,91
-Jony,17,93
-Json,21,85
-
-Then, the output of the program should be:
-[('John', '20', '90'), ('Jony', '17', '91'), ('Jony', '17', '93'), ('Json', '21', '85'), ('Tom', '19', '80')]'''
-
 li=[]
-for i in range(5):
-    tu=tuple()
-    name=input("enter your name")
-    x=name.split(',')
-    for j in x:
-        tu=tu+(j,)
-    li.append(tu)
-print(li)
-    
+for i in range(1,6):
+    t=tuple()
+    n=input("Enter the name")
+    sp=n.split(",")
+    for j in sp:
+        t=t+(j,)
+    li.append(t)
+names=[li[0][0],li[1][0],li[2][0],li[3][0],li[4][0]]
+names.sort()
+print(names)
+
--- trial.py
+++ trial.py
@@ -1,14 +1,2 @@
-li = ["SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY",
-      "THURSDAY", "FRIDAY", "SATURDAY"]
-user_Inp = int(input("Enter number of terms: "))
-t = 0
-d = 0
-z = 1
-for i in range(user_Inp):
-    t = t+d
-    d = d+1
-    if(i <= 3):
-        print(li[t])
-    else:
-        print(li[t - 7*z])
-        z += 1
+t=(1,2,3,4)
+print(sum(t))
