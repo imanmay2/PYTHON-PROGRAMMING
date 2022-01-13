08c0ec620d8e50682c417045fb7f752e996774af
Manmay <manmaychakarborty@gmail.com>
2022-01-14 01:04:29 +0530

    UPDATED VERSION 2.0(MID-NIGHT PRO)

--- Sorting name,age,height.py
+++ Sorting name,age,height.py
@@ -8,25 +8,26 @@
 #Json,21,85
 #Then the output will be like :[(John,20,90),(Jony,17,91),(Jony,17,93),(Json,21,85,(Tom,19,80))]
 li=[]
-for i in range(1,6):
+R=int(input("Enter the range"))
+for i in range(1,R+1):
     t=tuple()
     n=input("Enter the name")
     sp=n.split(",")
     for j in sp:
         t=t+(j,)
     li.append(t)
-for j in range(0,4):
-    for i in range(0,4):
+for j in range(0,R-1):
+    for i in range(0,R-1):
         if(li[i][0]>li[i+1][0]):
             s=li[i]
             li[i]=li[i+1]
             li[i+1]=s
-for i in range(0,4):
+for i in range(0,R-1):
     if(li[i][0]==li[i+1][0] and li[i][1]>li[i+1][1]):
         s1=li[i]
         li[i]=li[i+1]
         li[i+1]=s1
-for i in range(0,4):
+for i in range(0,R-1):
     if(li[i][1]==li[i+1][1] and li[i][2]>li[i+1][2]):
         s2=li[i]
         li[i]=li[i+1]
