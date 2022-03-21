d07d65cfe3e644021732d33444c50d7b5032be84
Manmay <manmaychakarborty@gmail.com>
2022-03-21 19:45:37 +0530

    hello

--- MODEM DEVICE program.py
+++ MODEM DEVICE program.py
@@ -1,10 +1,1 @@
-str1=input("enter the string").upper()
-li=str1.split()
-ct=0
-for i in range(len(li)):
-    for j in range(len(li[i])-1):
-        if (chr(ord(li[i][j])+1)) == li[i][j+1]:
-            ct=ct+1
-            print(li[i], end=" ")
-            break
-print("no. of such words are as follows",ct)
+
