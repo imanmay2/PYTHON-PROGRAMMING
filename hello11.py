f1e481bd3aa96fe2ed04910e2238b726b2bebcd0
Manmay <manmaychakarborty@gmail.com>
2022-03-21 19:45:26 +0530

    updated

--- hello11.py
+++ hello11.py
@@ -1,2 +1,4 @@
 #wap to accept a string and convert it to upper case and count the print the double
-#letter sequence that exists in that string
+#letter sequence that exists in that string.
+str1=input("enter the string you want")
+for
