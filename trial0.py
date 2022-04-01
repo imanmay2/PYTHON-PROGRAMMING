84b1b3a287c980bb90c81d9915cad0bbadd45b7c
Manmay <manmaychakarborty@gmail.com>
2022-04-01 21:46:50 +0530

    done

---
+++ trial0.py
@@ -0,0 +1,9 @@
+def check(s1,s2):
+    if(s1==s2):
+        return True
+    else:
+        return False
+s1=input("ENTER THE STRING: ")
+s2=input("ENTER THE STRING: ")
+print("RESULT IS AS FOLLOWS: ",check(s1,s2))
+
