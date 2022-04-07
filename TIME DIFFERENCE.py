28b22e3883b30e552582f4869bdb2d660f59c437
Manmay <manmaychakarborty@gmail.com>
2022-04-07 20:27:35 +0530

    module

--- TIME DIFFERENCE.py
+++ TIME DIFFERENCE.py
@@ -12,19 +12,19 @@ def time(t):
     return d1
 t1=int(input("ENTER THE TIME1: "))
 t2=int(input("ENTER THE TIME2: "))
-if((t1<1200 and t2>=1200) or (t1>=1200 and t2<1200)):
+if((t1<1200 and t2>1200) or (t1>1200 and t2<1200)):
     diff=time(t1)+time(t2)
     if(diff%100==60):
         diff=int(str((diff//100)+1)+str('00'))
-    if diff%100>60:
-        diff=int(str((diff//100)+1)+str(int(diff%100-60)))        
+    elif diff%100>60:
+        diff=int(str((diff//100)+1)+str((diff%100)-60))        
     print('THE DIFFERECE BETWEEN THE TIMINGS IS AS FOLLOWS: ',diff//100,"HOURS",diff%100,"MINUTES")
-elif((t1<1200 and t2<1200) or (t1>=1200 and t2>=1200)):
+elif((t1<1200 and t2<1200) or (t1>1200 and t2>1200)):
     diff=time(t1)-time(t2)
     if(diff<0):
         diff*=(-1)
     if(diff%100==60):
         diff=int(str((diff//100)+1)+str('00'))
-    if diff%100>60:
-        diff=int(str((diff//100)+1)+str(int(diff%100-60)))        
+    elif diff%100>60:
+        diff=int(str((diff//100)+1)+str((diff%100)-60))      
     print('THE DIFFERECE BETWEEN THE TIMINGS IS AS FOLLOWS: ',diff//100,"HOURS",diff%100,"MINUTES")
