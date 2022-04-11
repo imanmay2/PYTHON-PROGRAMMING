cb47f1d8fa83f2a722df7805b6fede4dc0adc4ef
Manmay <manmaychakarborty@gmail.com>
2022-04-11 15:06:10 +0530

    DONE

--- TIME DIFFERENCE.py
+++ TIME DIFFERENCE.py
@@ -1,6 +1,4 @@
+import DIFFTIME as t
 t1=int(input("ENTER THE FIRST TIME : "))
 t2=int(input("ENTER THE LAST TIME : "))
-diff=((t1//100)*60+(t1%100))-((t2//100)*60+(t2%100))
-if(diff<0):
-    diff*=(-1)
-print(diff//60," HOURS ",diff%60," MINS ")
+t.time_diff(t1,t2)
