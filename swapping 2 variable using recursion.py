56bf690379c14b6900d7ca2a1d4023a7ba633e4a
Manmay <manmaychakarborty@gmail.com>
2022-04-17 14:17:24 +0530

    corrected (mid-afternoon pro)

--- swapping 2 variable using recursion.py
+++ swapping 2 variable using recursion.py
@@ -4,16 +4,14 @@ def swap(a,b):
     if i==1:
         a=a+b
         swap(a,b)
-    if i==2:
+    elif i==2:
         b=a-b
         swap(a,b)
-    if i==3:
+    elif i==3:
         a=a-b
         swap(a,b)
-    if(i==4):
-        return a,b
-a=int(input("ENTER THE NUMBER 1: "))
-b=int(input("ENTER THE NUMBER 2: "))
+    elif(i==4):
+        print(a,b)
 global i
 i=0
-print(swap(a,b))
+swap(int(input("ENTER THE NUMBER 1: ")),int(input("ENTER THE NUMBER 2: ")))
