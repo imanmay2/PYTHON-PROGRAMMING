8d82e72016bd9f54b5af23c07277192b7232d70f
Manmay <manmaychakarborty@gmail.com>
2022-05-14 17:35:36 +0530

    package Q 4

---
+++ package Q4.py
@@ -0,0 +1,6 @@
+import sys
+sys.path.append("C:\Users\Windows 10\Desktop\PYTHON PROGRAMS\Conversion")
+import MassConversion from mass as m
+print(m.kgtotonne(10000))
+import LengthConversion from length as l
+print(l.feettoinches(4))
