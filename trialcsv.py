62a3d81b49b21c547737bbc46bf7c9ea8d658f42
Manmay <manmaychakarborty@gmail.com>
2022-06-14 21:33:34 +0530

    updated

--- trialcsv.py
+++ trialcsv.py
@@ -4,9 +4,9 @@ with open("result.csv","w") as f:
     wr.writerows(
         [
             ["Name","Points","Rank"],
-            ["Shradha",4500,23],
-            ["Nischay",4800,31],
-            ["Ali",4500,25],
-            ["Adi",5100,14]
+            ["Shradha","4500","23"],
+            ["Nischay","4800","31"],
+            ["Ali","4500","25"],
+            ["Adi","5100","14"]
         ]
     )
