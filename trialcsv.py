3836f530d63985f5a5027b8ed83e895e91fc1824
Manmay <manmaychakarborty@gmail.com>
2022-06-14 21:34:02 +0530

    updated

--- trialcsv.py
+++ trialcsv.py
@@ -1,12 +1,1 @@
 import csv
-with open("result.csv","w") as f:
-    wr=csv.writer(f)
-    wr.writerows(
-        [
-            ["Name","Points","Rank"],
-            ["Shradha","4500","23"],
-            ["Nischay","4800","31"],
-            ["Ali","4500","25"],
-            ["Adi","5100","14"]
-        ]
-    )
