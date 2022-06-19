38e013b67c4bfb5193541d06facf5dc2b64c4aa2
Manmay <manmaychakarborty@gmail.com>
2022-06-19 08:36:12 +0530

    fixed the bug

--- try.py
+++ try.py
@@ -1,11 +1,12 @@
 s=input("ENTER THE STRING: ")
 for i in s:
     if(i.islower()):
-        with open("lower.txt","w") as l:
+        with open("lower.txt","a") as l:
             l.write(i)
     elif(i.isupper()):
-        with open("upper.txt","w") as w:
+        with open("upper.txt","a") as w:
             w.write(i)
     else:
-        with open("others.txt","w") as h:
+        with open("others.txt","a") as h:
             h.write(i)
+    
