fc8ae613735ff8bc6e473114b9a11582fd9907f5
Manmay <manmaychakarborty@gmail.com>
2022-06-19 08:32:50 +0530

    exercise updated

--- try.py
+++ try.py
@@ -1,5 +1,11 @@
-file_name=input("ENTER THE FILE_NAME: ")
-with open("poem.txt","r") as y:
-    str1=y.read()
-with open(file_name,"w") as j:
-    j.write(str1)
+s=input("ENTER THE STRING: ")
+for i in s:
+    if(i.islower()):
+        with open("lower.txt","w") as l:
+            l.write(i)
+    elif(i.isupper()):
+        with open("upper.txt","w") as w:
+            w.write(i)
+    else:
+        with open("others.txt","w") as h:
+            h.write(i)
