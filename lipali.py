5b007ab4cd8340a65f0a1e873a195dfb4bc6f22c
Manmay <manmaychakarborty@gmail.com>
2022-06-21 08:10:13 +0530

    updated

--- lipali.py
+++ lipali.py
@@ -1,1 +1,9 @@
-import pickle
+
+with open("lines.txt") as f:
+    ct=1
+    for i in f.readlines():
+        for j in i:
+            if(j==' '):
+                ct+=1
+                if(ct==5):
+                    print(i)
