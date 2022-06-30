aa6ad4e4314a346a1701f9f3a8f4237376a4c5a3
Manmay <manmaychakarborty@gmail.com>
2022-06-30 21:40:46 +0530

    practising

--- image/FILE IO.py
+++ image/FILE IO.py
@@ -11,3 +11,14 @@ def hass():
 hass()        
 
 
+# copy text of one file to another and repeated space should be replaced by the single one  using def fucntions.
+def hello():
+    F=input("Enter the file name to copy from: ")
+    f=input("Enter the file name to be pasted simultaneously: ")
+    with open(F) as k:
+        li=k.read().split()
+        with open(f,"w") as K:
+            for  i in li:
+                K.write(i+" ")
+
+hello()
