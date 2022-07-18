1b989cc8733be14096f9d5f2ea9dd1810933b05c
Manmay <manmaychakarborty@gmail.com>
2022-07-18 07:12:43 +0530

    PI COMPUTATION DONE SUCCESSFULLY.(MORNING -PRO)

--- PROGRAMS/computation of the val of pi.py
+++ PROGRAMS/computation of the val of pi.py
@@ -1,1 +1,11 @@
-n=int(input("Enter the range upto which you wanna display the value of pi after decimal place: "))
+# To compute the value of pi upto 'n' decimal terms entered by the user.
+n=int(input("enter the range of the value of pi: "))
+pi="3.142857"
+d="142857"*n
+if(n<=6):
+    print(pi[0:n+2])
+elif(n>6):
+    print(pi+d[0:n-6])
+        
+    
+
