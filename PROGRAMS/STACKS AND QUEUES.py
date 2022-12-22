19597d27ba3a2767a89bba70ed5b0bb5280a9ce8
Manmay <manmaychakarborty@gmail.com>
2022-12-23 01:27:22 +0530

    updated queues

--- PROGRAMS/STACKS AND QUEUES.py
+++ PROGRAMS/STACKS AND QUEUES.py
@@ -47,3 +47,53 @@ while True:
         break
     else:
         print("INAVALID INPUT!!!PLEASE ENTER FROM 1 TO 5!!!! TRY AGAIN AFTER SOMETIME!!!")  
+
+
+# IMPLEMENTATION OF THE  QUEUES OPERATIONS...........
+def enque(Qu,item,n):
+    if(len(Qu)==n):
+        print("Overflow")
+    else:
+        Qu.append(item)
+def pop(Qu):
+    if(Qu==[]):
+        print("Queues is empty")
+    else:
+        print("Poped element of the Queues is: ",Qu.pop())
+def peek(Qu):
+    if(Qu==[]):
+        print("Queue is empty")
+    else:
+        print("Peek value of the Queues: ",Qu[len(Qu)-1])
+def display(Qu):
+    if(Qu==[]):
+        print("Queues is empty")
+    else:
+        print("Queues is as follows: ")
+        for i in range(len(Qu)-1,-1,-1):
+            print(Qu[i])
+# main code starts now.........
+Qu=[]
+n=int(input("Enter the strength of the Qu: "))
+while True:
+    print("Queues OPERATIONS")
+    print("1. ENQUEUE")
+    print("2. DEQUEUE")
+    print("3. TOPMOST VALUE")
+    print("4. DISPLAY QUEUES")
+    print("5. EXIT!!")
+    ch=int(input("Enter the choice: "))
+    if(ch==1):
+        item=int(input("Enter the item to be enqueue into the Queue: "))
+        push(Qu,item,n)  
+    elif(ch==2):
+        pop(Qu)
+    elif(ch==3):
+        peek(Qu)
+    elif(ch==4):
+        display(Qu)
+    elif(ch==5):
+        print("BYE BYE!!!!")
+        break
+    else:
+        print("INAVALID INPUT!!!PLEASE ENTER FROM 1 TO 5!!!! TRY AGAIN AFTER SOMETIME!!!")  
