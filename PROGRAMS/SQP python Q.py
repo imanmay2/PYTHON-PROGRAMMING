6bfa0e7493d0d9b2a9e7e77dfa534b41cde457a7
Manmay <manmaychakarborty@gmail.com>
2023-01-26 01:57:22 +0530

    updated

---
+++ PROGRAMS/SQP python Q.py
@@ -0,0 +1,12 @@
+# Write a method COUNTLINES() in python to read lines from the text file 'TESTFILE.TXT'  
+# and display the lines which are not starting with any vowel.
+def COUNTLINES():
+    with open("TESTFILE.TXT") as f:
+        v='aeiou'
+        ct=0
+        for i in f.readlines():
+            if(i[0] in v):
+                ct+=1
+    return ct
+print("THE NUMBER OF LINES NOT STARTING WITH ANY VOWEL IS: ",COUNTLINES())
+
