c6a9f5ff27002ac7039efcbaacba1366fff294ea
Manmay <manmaychakarborty@gmail.com>
2023-01-26 02:12:36 +0530

    updated

--- PROGRAMS/SQP python Q.py
+++ PROGRAMS/SQP python Q.py
@@ -9,4 +9,21 @@ def COUNTLINES():
                 ct+=1
     return ct
 
+# Write a function ETCount() in Python, which should read each character of a text file "TESTFILE.TXT" and then
+# count and display the count of occurence of alphabets E and T insividually (including the small cases e and t too)
 
+def ETCount():
+    with open("TEST.TXT",'w') as f1:
+        str2='Today is a pleasant day It might rain rain today It is mentioned on weather sites.'
+        f1.write(str2)
+    with open("TEST.TXT",'r') as f2:
+        ct_t=0
+        ct_e=0
+        for i in f.read():
+            if(i=='e' or i=='E'):
+                ct_e+=1
+            elif(i=='t' or i=='T'):
+                ct_t+=1
+    print("E or e: ",ct_e)
+    print("T or t: ",ct_t)
+ETCount()
