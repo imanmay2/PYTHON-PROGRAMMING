bf7cf8c58e0e5eaf91b92d02207dce8db065ced1
Manmay <manmaychakarborty@gmail.com>
2023-02-02 18:42:00 +0530

    CHALLENGE #2 DONE

--- PROGRAMS/CHALLENGES.py
+++ PROGRAMS/CHALLENGES.py
@@ -11,11 +11,22 @@ def func1():
         print(i,end=',')
 
 
-#2. WAP to convert infix to postfix and reverse of it.
-#3. WAP to check precedence order of operators. Eg. Input => * and + … Output => * is in higher preceedance.
-#   Eg: Input => * and / Out[ut => Same preceedance …
-#4. WAP to display output in tabular format
+# WAP that will accept a person's age and check for its vote eligiblity.
+def check_eligiblity():
+    age=int(input("Enter the age"))
+    while(age>18):
+        print("PERSON IS ELIGIBLE FOR VOTE")
+        break
+    while(age<18):
+        print("PERSON IS NOT ELIGIBLE FOR VOTE")
+        print("HE WILL BE ELIGIBLE AFTER",18-age,"YEARS")
+        break
+check_eligiblity()
+#3. WAP to convert infix to postfix and reverse of it.
+#4. WAP to check precedence order of operators. Eg. Input => * and + … Output => * is in higher preceedance.
+#   Eg: Input => * and / Output => Same preceedance …
+#5. WAP to display output in tabular format
 #   (as we see in MySQL output of select command) for inputs by user.
-#5. WAP that will accept a CARBON CONTAINING COMPOUND and display the IUPAC NOMENCLATURE of that compound...
+#6. WAP that will accept a CARBON CONTAINING COMPOUND and display the IUPAC NOMENCLATURE of that compound...
 #   For e.g: C2H5OH as Ethanol
 #   For e.g: CH4 as Methane
