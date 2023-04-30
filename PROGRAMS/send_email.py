c5f038acbe8f36be211e158c91463e07cf0a9ce5
MANMAY CHAKRABORTY <manmaychakarborty@gmail.com>
2023-04-30 18:40:21 +0530

    Update send_email.py

--- PROGRAMS/send_email.py
+++ PROGRAMS/send_email.py
@@ -2,5 +2,5 @@
 import smtplib
 server=smtplib.SMTP('smtp.gmail.com',587)
 server.starttls()
-server.login('manmaychakarborty@gmail.com','1@3$5^7*')
+server.login('manmaychakarborty@gmail.com','')
 server.sendmail('manmaycoder@gmail.com','manmaychakarborty@gmail.com','hello')
