ac1e3b735d8207f069e175e446b1da277dbc5589
manmay2 <imanmay2@gmail.com>
2024-10-06 16:06:40 +0530

    yet to be done

---
+++ PROGRAMS/IPS_Q.py
@@ -0,0 +1,7 @@
+# Write a program that inputs 2 string 
+#                 s="ab#d"   
+#                 t="ac#d"   
+# Pop the character # and the character before # , and if the character become  same then return True.
+
+
+
