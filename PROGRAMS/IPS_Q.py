ae9a9098335ce9c36d18db7bfbc8eb9a61e8275e
manmay2 <imanmay2@gmail.com>
2024-10-10 15:19:40 +0530

    matlab code updated

--- PROGRAMS/IPS_Q.py
+++
@@ -1,7 +0,0 @@
-# Write a program that inputs 2 string 
-#                 s="ab#d"   
-#                 t="ac#d"   
-# Pop the character # and the character before # , and if the character become  same then return True.
-
-
-
